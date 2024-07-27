68810627369dd08efcf5765491bbcd343f7eed16
ABU TAHER <abutaher267@gmail.com>
2024-07-27 15:11:45 +0600

    change live servr url

--- src/TwitterDMConfig.js
+++ src/TwitterDMConfig.js
@@ -1,7 +1,7 @@
 /* eslint-disable class-methods-use-this */
 // const NODE_BASE_URL_LOCAL = 'https://ccb9-116-206-253-168.ngrok-free.app'; // localhost
 const NODE_BASE_URL_LOCAL = 'http://localhost:5000'; // localhost
-const NODE_BASE_URL = 'https://fly-dms-server.onrender.com'; // live
+const NODE_BASE_URL = 'https://reddit-server-2ptn.onrender.com'; // live
 
 class TwitterDMConfig {
     getNodeUrl() {
