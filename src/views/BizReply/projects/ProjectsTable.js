f83d06158a3bc574424b18cac68c2734450c220a
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2024-08-14 20:27:30 +0600

    redesign projects page

--- src/views/BizReply/projects/ProjectsTable.js
+++ src/views/BizReply/projects/ProjectsTable.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-alert */
 /* eslint-disable no-restricted-globals */
 import { Box } from '@mui/system';
-import { IconTrash } from '@tabler/icons';
+import { IconTrash, IconEdit } from '@tabler/icons';
 import { projectRemoving } from 'features/project/projectActions';
 // import BizReplyConfig from 'BizReplyConfig';
 
@@ -9,8 +9,9 @@ import useAuth from 'hooks/useAuth';
 import { toast } from 'react-toastify';
 import axios from 'utils/axios';
 import { Card, CardContent, Grid, Typography } from '@mui/material';
-import redditFeeds from 'assets/images/reddit-feeds.jpg';
+import redditFeeds from 'assets/images/demoWebsite.png';
 import { subsctriptionCreditsSetter } from 'features/subscription/subscriptionActions';
+import BRButton from 'ui-component/bizreply/BRButton';
 
 const ProjectTable = ({
     // setProjects,
@@ -39,12 +40,17 @@ const ProjectTable = ({
             });
     };
 
+    const editProject = async (id) => {
+        console.log(id);
+        toast('We are working on this feature', { autoClose: 2500, type: 'warning' });
+    };
+
     return (
         <Box>
             <Grid container spacing={2}>
                 {projects?.map?.((item) => (
                     <Grid key={item._id} item xs={12} sm={6} md={4}>
-                        <ProjectCard {...item} deleteProject={deleteProject} />
+                        <ProjectCard {...item} deleteProject={deleteProject} editProject={editProject} />
                     </Grid>
                 ))}
             </Grid>
@@ -58,45 +64,71 @@ export default ProjectTable;
 
 // ==============================|| SKELETON - EARNING CARD ||============================== //
 
-const ProjectCard = ({ thumbnail = redditFeeds, brandName, domain, shortDescription, deleteProject, _id }) => (
+const ProjectCard = ({ thumbnail = redditFeeds, brandName, domain, shortDescription, deleteProject, _id, editProject }) => (
     <Card>
-        <CardContent sx={{ p: 0 }}>
+        <CardContent sx={{ p: 0, fontWeight: '500' }}>
             <Box sx={{ position: 'relative' }}>
-                <img src={thumbnail} alt="Reddit Feeds" style={{ maxWidth: '100%' }} />
+                <img src={thumbnail} alt="Reddit Feeds" style={{ maxWidth: '100%', padding: '15px', borderRadius: '25px' }} />
                 <Typography
                     style={{
                         cursor: 'pointer',
                         position: 'absolute',
                         display: 'flex',
                         justifyContent: 'center',
-                        height: '25px',
-                        width: '25px',
+                        height: '35px',
+                        width: '35px',
                         alignItems: 'center',
-                        top: '10px',
-                        right: '10px',
-                        background: ' #ddd',
-                        color: 'red',
+                        bottom: '30px',
+                        right: '70px',
+                        background: ' #DDDDDD',
+                        color: '#6E7478',
                         borderRadius: '50%'
                     }}
                     onClick={() => deleteProject(_id)}
                 >
-                    <IconTrash size={16} />
+                    <IconTrash size={20} />
+                </Typography>
+                <Typography
+                    style={{
+                        cursor: 'pointer',
+                        position: 'absolute',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        height: '35px',
+                        width: '35px',
+                        alignItems: 'center',
+                        bottom: '30px',
+                        right: '30px',
+                        background: ' #DDDDDD',
+                        color: '#6E7478',
+                        borderRadius: '50%'
+                    }}
+                    onClick={() => editProject(_id)}
+                >
+                    <IconEdit size={20} />
                 </Typography>
             </Box>
             <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1, p: 3 }}>
                 <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                    <Typography sx={{ fontWeight: 'bold' }}>Brand Name :</Typography>
+                    <Typography sx={{ color: '#6E7478' }}>Brand Name :</Typography>
                     <Typography>{brandName}</Typography>
                 </Box>
                 <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                    <Typography sx={{ fontWeight: 'bold' }}>Domain :</Typography>
+                    <Typography sx={{ color: '#6E7478' }}>Domain :</Typography>
                     <Typography>{domain}</Typography>
                 </Box>
                 <Box sx={{ display: '', justifyContent: 'space-between' }}>
-                    <Typography sx={{ fontWeight: 'bold', mb: 2 }}>Description :</Typography>
+                    <Typography sx={{ color: '#6E7478', mb: 2 }}>Description :</Typography>
                     <Typography>{shortDescription}</Typography>
                 </Box>
             </Box>
+            <BRButton
+                sx={{ height: '40px', marginTop: '25px', width: '90%', marginLeft: '5%' }}
+                variant="contained"
+                // onClick={toggleProjectCreateModalCtrl()}
+            >
+                View Details
+            </BRButton>
         </CardContent>
     </Card>
 );
--- src/views/BizReply/projects/index.jsx
+++ src/views/BizReply/projects/index.jsx
@@ -3,28 +3,16 @@ import { DEFAULT_BUTTON_COLOR_CODE } from 'config';
 import ProjectsTable from './ProjectsTable';
 import { useSelector } from 'react-redux';
 import { toggleProjectCreateModalCtrl } from 'features/project/projectActions';
+import BRButton from 'ui-component/bizreply/BRButton';
+import { FaAngleDown, FaAngleUp } from 'react-icons/fa6';
+import BRProjectSelection from 'ui-component/bizreply/BRProjectSelection';
 
 const Projects = () => {
     const { projects } = useSelector((state) => state.project);
 
     return (
         <>
-            <Card sx={{ mb: 5 }}>
-                <CardContent style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                    <div>
-                        <Typography variant="h2" style={{ marginRight: 'auto' }}>
-                            Projects
-                        </Typography>
-                    </div>
-                    <Button
-                        onClick={toggleProjectCreateModalCtrl()}
-                        variant="outlined"
-                        style={{ background: DEFAULT_BUTTON_COLOR_CODE, color: '#fff' }}
-                    >
-                        Add Project
-                    </Button>
-                </CardContent>
-            </Card>
+            <BRProjectSelection {...{ toggleProjectCreateModalCtrl }} />
             <ProjectsTable {...{ projects }} />
         </>
     );
