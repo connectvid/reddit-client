07d36bf622e911dadfada4ee981bb61c24f61972
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2024-08-16 16:04:23 +0600

    b

--- src/ui-component/bizReply/steps/Step3.js
+++ src/ui-component/bizReply/steps/Step3.js
@@ -1,14 +1,98 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Stepper3 from './stepper/Stepper3';
-import { Grid } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 import BRButton from '../BRButton';
+import reddit from 'assets/images/platforms/reddit.png';
+import linkedin from 'assets/images/platforms/linkedin.png';
+import quora from 'assets/images/platforms/quora.png';
+import twitter from 'assets/images/platforms/twitter.png';
+import { FaRegSquare } from 'react-icons/fa';
+import { FaCheck } from 'react-icons/fa6';
+import { Box } from '@mui/system';
+import { useSelector } from 'react-redux';
 
 const Step3 = ({ setStep }) => {
-    console.log('Step 2');
+    const platformsSrc = {
+        'reddit.com': reddit,
+        'linkedin.com': linkedin,
+        'quora.com': quora,
+        'twitter.com': twitter
+    };
+    const { project } = useSelector((state) => state.project);
+    const [selectedPlatforms, setselectedPlatforms] = useState([]);
+    const handleSelectedPlatform = (platform) => {
+        console.log(platform);
+        if (!selectedPlatforms.includes(platform)) {
+            setselectedPlatforms([...selectedPlatforms, platform]);
+        } else {
+            setselectedPlatforms(selectedPlatforms.filter((item) => item !== platform));
+        }
+    };
+
     return (
         <div>
             <Stepper3 />
-            <p>thire screen</p>
+            <Typography sx={{ color: '#000', fontWeight: 500, fontSize: '16px', mb: 3 }}>
+                Select social profile (s) where you want to see posts from.
+            </Typography>
+
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                {project?.platforms?.map?.((platform) => (
+                    <Typography
+                        key={platform}
+                        component="div"
+                        sx={{
+                            cursor: 'pointer',
+                            p: 0,
+                            maxWidth: '154px',
+                            // border: `1px solid ${selectedPlatform === platform ? '#0C22E5' : '#CCD3D9'}`,
+                            border: `1px solid ${selectedPlatforms.includes(platform) ? '#0C22E5' : '#CCD3D9'}`,
+                            minHeight: '75px',
+                            display: 'flex',
+                            flex: 1,
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            borderRadius: '10px',
+                            position: 'relative'
+                        }}
+                        onClick={() => handleSelectedPlatform(platform)}
+                    >
+                        <Typography sx={{ position: 'absolute', top: '10px', right: '10px' }}>
+                            {selectedPlatforms.includes(platform) ? (
+                                <Typography
+                                    component="span"
+                                    sx={{
+                                        height: '14px',
+                                        width: '14px',
+                                        borderRadius: '4px',
+                                        display: 'flex',
+                                        justifyContent: 'center',
+                                        alignItems: 'center',
+                                        background: 'linear-gradient(92.84deg, #0C22E5 0%, #2A98D5 96.82%)'
+                                    }}
+                                >
+                                    <FaCheck size={10} color="#fff" />
+                                </Typography>
+                            ) : (
+                                <FaRegSquare size={14} color="#667185" />
+                            )}
+                        </Typography>
+                        <img
+                            src={platformsSrc[platform]}
+                            alt={platform}
+                            style={{
+                                width: '65%',
+                                marginTop: '10px'
+                            }}
+                        />
+                    </Typography>
+                )) || ''}
+            </Box>
+
+            <Typography sx={{ color: '#6E7478', fontWeight: 500, fontSize: '14px', mb: 3, mt: 2 }}>
+                <span style={{ color: '#000', textDecoration: 'underline' }}>Skip and finish the step for now,</span> you’ll be able to
+                connect your socials later.
+            </Typography>
             <Grid sx={{ display: 'flex', justifyContent: 'flex-end', gap: '10px' }}>
                 <BRButton variant="outlined" style={{ width: '210px' }} onClick={() => setStep(2)}>
                     Back to Previous
--- src/ui-component/bizReply/steps/Step4.js
+++ src/ui-component/bizReply/steps/Step4.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import Stepper3 from './stepper/Stepper3';
-import { Grid } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 import BRButton from '../BRButton';
+import congratulations from 'assets/images/congratulations.png';
 
 const Step4 = ({ setStep }) => {
     console.log('Step 2');
     return (
         <div>
             <Stepper3 />
-            <p>fourth screen</p>
+            <img style={{ width: '10%', margin: '30px 45% 10px' }} src={congratulations} alt="congratulations" />
+            {/* */}
+            {/*   */}
+            <Typography sx={{ color: '#000', fontWeight: 500, fontSize: '19px', textAlign: 'center' }}>Congratulations!</Typography>
+            <Typography sx={{ color: '#6E7478', fontWeight: 500, fontSize: '16px', textAlign: 'center', mt: 1, mb: 2 }}>
+                You’ve successfully created a project 🚀
+            </Typography>
             <Grid sx={{ display: 'flex', justifyContent: 'flex-end', gap: '10px' }}>
-                <BRButton variant="outlined" style={{ width: '210px' }} onClick={() => setStep(3)}>
+                {/* <BRButton variant="outlined" style={{ width: '210px' }} onClick={() => setStep(3)}>
                     Create Another Project
-                </BRButton>
+                </BRButton> */}
                 <BRButton variant="contained" style={{ width: '180px' }} onClick={() => setStep(3)}>
                     Go to Projects Page
                 </BRButton>
