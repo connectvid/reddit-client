f2a94c2b9ef5fcd30ebf9c47e37dcf51d2e6372f
ABU TAHER <abutaher267@gmail.com>
2024-08-20 01:59:28 +0600

    dev config change

--- src/BizReplyConfig.js
+++ src/BizReplyConfig.js
@@ -1,19 +1,17 @@
 /* eslint-disable class-methods-use-this */
 // const NODE_BASE_URL_LOCAL = 'https://ccb9-116-206-253-168.ngrok-free.app'; // localhost
 const NODE_BASE_URL_LOCAL = 'http://localhost:5000'; // localhost
-export const NODE_BASE_URL_SOCKET_LOCAL = 'http://localhost:5000'; // localhost
-export const NODE_BASE_URL = 'https://reddit-server-2ptn.onrender.com'; // dev
+export const NODE_BASE_URL_SOCKET_LOCAL = 'http://localhost:5001'; // localhost
+export const NODE_BASE_URL = 'https://dev-reddit-server.onrender.com'; // dev
 const NODE_BASE_URL_MAIN = 'https://reddit-server-2ptn.onrender.com'; // live
-const originWise = ['https://gv-reddit.netlify.app', 'https://app.bizreply.co'].includes(window.location.origin)
-    ? NODE_BASE_URL_MAIN
-    : NODE_BASE_URL;
+
 class BizReplyConfig {
     getNodeUrl() {
-        return process.env.NODE_ENV === 'development' ? NODE_BASE_URL_LOCAL : originWise;
+        return process.env.NODE_ENV === 'development' ? NODE_BASE_URL_LOCAL : NODE_BASE_URL;
     }
 
     getNodeSocketUrl() {
-        return process.env.NODE_ENV === 'development' ? NODE_BASE_URL_SOCKET_LOCAL : originWise;
+        return process.env.NODE_ENV === 'development' ? NODE_BASE_URL_SOCKET_LOCAL : NODE_BASE_URL_LOCAL;
     }
 
     tweetPerPage = 30;
