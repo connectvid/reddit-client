fc1477fe259e3a5bee57c5d3ca9067166db19ba6
ABU TAHER <abutaher267@gmail.com>
2024-08-29 11:45:47 +0600

    add dev subdomain

--- src/BizReplyConfig.js
+++ src/BizReplyConfig.js
@@ -2,7 +2,7 @@
 // const NODE_BASE_URL_LOCAL = 'https://ccb9-116-206-253-168.ngrok-free.app'; // localhost
 const NODE_BASE_URL_LOCAL = 'http://localhost:5000'; // localhost
 export const NODE_BASE_URL_SOCKET_LOCAL = 'http://localhost:5001'; // localhost
-export const NODE_BASE_URL = 'https://dev-reddit-server.onrender.com'; // dev
+export const NODE_BASE_URL = 'https://dev.bizreply.co'; // dev
 // export const NODE_BASE_URL = 'https://dev.bizreply.co'; // dev
 const NODE_BASE_URL_MAIN = 'https://api1.bizreply.co'; // live
 
