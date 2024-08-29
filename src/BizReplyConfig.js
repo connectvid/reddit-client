6f7259720807f53db921a7c86a19c9ce03c8550e
ABU TAHER <abutaher267@gmail.com>
2024-08-29 11:59:20 +0600

    api url with socket

--- src/BizReplyConfig.js
+++ src/BizReplyConfig.js
@@ -1,18 +1,18 @@
 /* eslint-disable class-methods-use-this */
 // const NODE_BASE_URL_LOCAL = 'https://ccb9-116-206-253-168.ngrok-free.app'; // localhost
-const NODE_BASE_URL_LOCAL = 'http://localhost:5000'; // localhost
+// const NODE_BASE_URL_LOCAL = 'http://localhost:5000'; // localhost
 export const NODE_BASE_URL_SOCKET_LOCAL = 'http://localhost:5001'; // localhost
-export const NODE_BASE_URL = 'https://dev.bizreply.co'; // dev
 // export const NODE_BASE_URL = 'https://dev.bizreply.co'; // dev
-const NODE_BASE_URL_MAIN = 'https://api1.bizreply.co'; // live
+// export const NODE_BASE_URL = 'https://dev.bizreply.co'; // dev
+// const NODE_BASE_URL_MAIN = 'https://api1.bizreply.co'; // live
 
 class BizReplyConfig {
     getNodeUrl() {
-        return process.env.NODE_ENV === 'development' ? NODE_BASE_URL_LOCAL : NODE_BASE_URL_MAIN;
+        return process.env.REACT_APP_APP_API_URL;
     }
 
     getNodeSocketUrl() {
-        return process.env.NODE_ENV === 'development' ? NODE_BASE_URL_SOCKET_LOCAL : NODE_BASE_URL_MAIN;
+        return process.env.NODE_ENV === 'development' ? process.env.REACT_APP_APP_API_SOCKET_URL : process.env.REACT_APP_APP_API_URL;
     }
 
     tweetPerPage = 30;
--- src/socket.js
+++ src/socket.js
@@ -1,8 +1,7 @@
 /*  eslint-disable-next-line import/no-unresolved */
 import { io } from 'socket.io-client';
-import BizReplyConfig from 'BizReplyConfig';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = BizReplyConfig.getNodeSocketUrl();
+const URL = process.env.NODE_ENV === 'development' ? process.env.REACT_APP_APP_API_SOCKET_URL : process.env.REACT_APP_APP_API_URL;
 
 export default io(URL, { autoConnect: false });
