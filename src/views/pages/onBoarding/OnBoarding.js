4b32b802f9d3c2792a9b76693ac15df2ff626142
ABU TAHER <abutaher267@gmail.com>
2024-09-02 17:35:42 +0600

    update

--- src/views/pages/onBoarding/OnBoarding.js
+++ src/views/pages/onBoarding/OnBoarding.js
@@ -1,28 +1,10 @@
-import { Modal } from '@mui/material';
-import { useSelector } from 'react-redux';
-import { clearingError, toggleProjectCreateModalCtrl } from 'features/project/projectActions';
-import NewProject from 'views/BizReply/projects/NewProject';
+import { toggleProjectCreateModalCtrl } from 'features/project/projectActions';
 import React from 'react';
-import { toast } from 'react-toastify';
 
 export default function () {
-    const {
-        project: { error, showProjectCreateModal }
-    } = useSelector((state) => state);
-
     React.useEffect(() => {
         toggleProjectCreateModalCtrl()();
     }, []);
-    React.useEffect(() => {
-        if (error) {
-            toast.warning(error);
-            clearingError()();
-        }
-    }, [error]);
+
     return <></>;
-    // return (
-    //     <Modal open={showProjectCreateModal}>
-    //         <NewProject />
-    //     </Modal>
-    // );
 }
