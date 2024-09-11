40eac1f213447071780660fccbcbc06bedae02c2
ABU TAHER <abutaher267@gmail.com>
2024-09-11 16:21:56 +0600

    change prompt card

--- src/views/BizReply/prompts/CardDetails.js
+++ src/views/BizReply/prompts/CardDetails.js
@@ -18,93 +18,29 @@ const CardDetails = ({ language, tone, reply_character_limit, description, name,
     // : description?.length > allowDescriptionLength
     // ? `${description.substring(0, allowDescriptionLength)}`
     // : description;
+
+    // src  title       subtitle
+    // Language, Language language
+    // Tone Tone tone
+    // style={{ width: '20px', marginTop: '-22px' }} 2
+    // Character, Character, reply_character_limit
     return (
         <Box>
             <Box sx={{ display: 'flex', mb: 2 }}>
                 <img src={Prompt} alt="Prompt" style={{ width: '20px' }} />
                 <Typography sx={{ fontWeight: 700, fontSize: '16px', color: '#000', ml: 2 }}>Prompt Name: {name}</Typography>
             </Box>
-
             <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', gap: '10px' }}>
-                <Box
-                    sx={{
-                        fontWeight: 700,
-                        fontSize: '16px',
-                        color: '#000',
-                        mb: 2,
-                        background: '#f4f8fd',
-                        border: '1px solid #0C22E5',
-                        borderRadius: '8px',
-                        flex: '1',
-                        height: '100%',
-                        padding: '8px',
-                        display: 'flex',
-                        gap: '5px',
-                        alignItems: 'center'
+                <Tags {...{ src: Language, title: 'Language', subtitle: language }} />
+                <Tags {...{ src: Tone, title: 'Tone', subtitle: tone, srcStyle: { width: '18px', marginTop: '-8px' } }} />
+                <Tags
+                    {...{
+                        src: Character,
+                        title: 'Character',
+                        subtitle: reply_character_limit,
+                        srcStyle: { width: '14px', marginTop: '-8px' }
                     }}
-                >
-                    <img src={Language} alt="Prompt" style={{ width: '20px', marginTop: '-22px' }} />
-                    <Box>
-                        <Typography
-                            sx={{ color: '#6E7478', fontWeight: '300', textTransform: 'uppercase', fontSize: '12px', marginBottom: '8px' }}
-                        >
-                            Language
-                        </Typography>
-                        <Typography>{language}</Typography>
-                    </Box>
-                </Box>
-                <Box
-                    sx={{
-                        fontWeight: 700,
-                        fontSize: '16px',
-                        color: '#000',
-                        mb: 2,
-                        background: '#f4f8fd',
-                        border: '1px solid #0C22E5',
-                        borderRadius: '8px',
-                        flex: '1',
-                        height: '100%',
-                        padding: '8px',
-                        display: 'flex',
-                        gap: '5px'
-                    }}
-                >
-                    <img src={Tone} alt="Prompt" style={{ width: '20px', marginTop: '-22px' }} />
-                    <Box>
-                        <Typography
-                            sx={{ color: '#6E7478', fontWeight: '300', textTransform: 'uppercase', fontSize: '12px', marginBottom: '8px' }}
-                        >
-                            Tone
-                        </Typography>
-                        <Typography>{tone}</Typography>
-                    </Box>
-                </Box>
-                <Box
-                    sx={{
-                        fontWeight: 700,
-                        fontSize: '16px',
-                        color: '#000',
-                        mb: 2,
-                        background: '#f4f8fd',
-                        border: '1px solid #0C22E5',
-                        borderRadius: '8px',
-                        flex: '1',
-                        height: '100%',
-                        padding: '8px',
-                        display: 'flex',
-                        gap: '5px'
-                    }}
-                >
-                    <img src={Character} alt="Prompt" style={{ width: '14px', marginTop: '-22px' }} />
-                    <Box>
-                        <Typography
-                            sx={{ color: '#6E7478', fontWeight: '300', textTransform: 'uppercase', fontSize: '12px', marginBottom: '8px' }}
-                        >
-                            Character
-                        </Typography>
-                        <Typography>{reply_character_limit}</Typography>
-                    </Box>
-                </Box>
+                />
             </Box>
 
             <Typography sx={{ fontWeight: 700, fontSize: '16px', color: '#000', mb: 0.5 }}>Description:</Typography>
@@ -154,3 +90,151 @@ const CardDetails = ({ language, tone, reply_character_limit, description, name,
 };
 
 export default CardDetails;
+
+const Tags = ({ src, srcStyle = {}, title, subtitle }) => {
+    return (
+        <Box
+            sx={{
+                fontWeight: 700,
+                fontSize: '16px',
+                color: '#000',
+                mb: 2,
+                background: '#f4f8fd',
+                border: '1px solid #0C22E5',
+                borderRadius: '8px',
+                // flex: '1',
+                width: '100%',
+                height: '100%',
+                padding: '8px',
+                // display: 'flex',
+                gap: 0.5
+                // alignItems: 'center'
+            }}
+        >
+            <Box
+                sx={{
+                    alignItems: 'center',
+                    display: 'flex',
+                    justifyContent: 'space-between'
+                }}
+            >
+                <img src={src} alt="Prompt" style={{ width: '20px', ...srcStyle }} />
+                <Typography sx={{ color: '#6E7478', fontWeight: '300', textTransform: 'uppercase', fontSize: '10px', marginBottom: '8px' }}>
+                    {title}
+                </Typography>
+            </Box>
+            <Typography sx={{ fontSize: '12.5px', textAlign: 'right' }}>{subtitle}</Typography>
+        </Box>
+    );
+};
+
+{
+    /* <Box
+            sx={{
+                fontWeight: 700,
+                fontSize: '16px',
+                color: '#000',
+                mb: 2,
+                background: '#f4f8fd',
+                border: '1px solid #0C22E5',
+                borderRadius: '8px',
+                flex: '1',
+                height: '100%',
+                padding: '8px',
+                display: 'flex',
+                gap: '5px',
+                alignItems: 'center'
+            }}
+        >
+            <img src={src} alt="Prompt" style={{ width: '20px', marginTop: '-22px', ...srcStyle }} />
+            <Box>
+                <Typography sx={{ color: '#6E7478', fontWeight: '300', textTransform: 'uppercase', fontSize: '12px', marginBottom: '8px' }}>
+                    {title}
+                </Typography>
+                <Typography>{subtitle}</Typography>
+            </Box>
+        </Box> */
+}
+{
+    /* <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', gap: '10px' }}>
+<Box
+    sx={{
+        fontWeight: 700,
+        fontSize: '16px',
+        color: '#000',
+        mb: 2,
+        background: '#f4f8fd',
+        border: '1px solid #0C22E5',
+        borderRadius: '8px',
+        flex: '1',
+        height: '100%',
+        padding: '8px',
+        display: 'flex',
+        gap: '5px',
+        alignItems: 'center'
+    }}
+>
+    <img src={Language} alt="Prompt" style={{ width: '20px', marginTop: '-22px' }} />
+    <Box>
+        <Typography
+            sx={{ color: '#6E7478', fontWeight: '300', textTransform: 'uppercase', fontSize: '12px', marginBottom: '8px' }}
+        >
+            Language
+        </Typography>
+        <Typography>{language}</Typography>
+    </Box>
+</Box>
+<Box
+    sx={{
+        fontWeight: 700,
+        fontSize: '16px',
+        color: '#000',
+        mb: 2,
+        background: '#f4f8fd',
+        border: '1px solid #0C22E5',
+        borderRadius: '8px',
+        flex: '1',
+        height: '100%',
+        padding: '8px',
+        display: 'flex',
+        gap: '5px'
+    }}
+>
+    <img src={Tone} alt="Prompt" style={{ width: '20px', marginTop: '-22px' }} />
+    <Box>
+        <Typography
+            sx={{ color: '#6E7478', fontWeight: '300', textTransform: 'uppercase', fontSize: '12px', marginBottom: '8px' }}
+        >
+            Tone
+        </Typography>
+        <Typography>{tone}</Typography>
+    </Box>
+</Box>
+<Box
+    sx={{
+        fontWeight: 700,
+        fontSize: '16px',
+        color: '#000',
+        mb: 2,
+        background: '#f4f8fd',
+        border: '1px solid #0C22E5',
+        borderRadius: '8px',
+        flex: '1',
+        height: '100%',
+        padding: '8px',
+        display: 'flex',
+        gap: '5px'
+    }}
+>
+    <img src={Character} alt="Prompt" style={{ width: '14px', marginTop: '-22px' }} />
+    <Box>
+        <Typography
+            sx={{ color: '#6E7478', fontWeight: '300', textTransform: 'uppercase', fontSize: '12px', marginBottom: '8px' }}
+        >
+            Character
+        </Typography>
+        <Typography>{reply_character_limit}</Typography>
+    </Box>
+</Box>
+</Box> */
+}
