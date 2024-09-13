f1ce5d0106ba3e24d8c49c91782f251ea7716646
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2024-09-13 17:51:16 +0600

    added icons

--- src/views/BizReply/mentions/PostCard/index.js
+++ src/views/BizReply/mentions/PostCard/index.js
@@ -16,7 +16,7 @@ import replaceDomainWithLink from 'utils/replaceDomainWithLink';
 import { subsctriptionCreditsSetter } from 'features/subscription/subscriptionActions';
 import { toast } from 'react-toastify';
 import errorMsgHelper from 'utils/errorMsgHelper';
-import { IconBrandReddit } from '@tabler/icons';
+import { IconBrandReddit, IconBrandTiktok } from '@tabler/icons';
 import { LinkedIn, Twitter, Facebook, Instagram, Pinterest } from '@mui/icons-material';
 import IconQuora from 'ui-component/icons/IconQuora';
 import { keywordColors } from 'data';
@@ -370,5 +370,9 @@ const SocialIcons = ({ platform }) => {
     if (platform === 'quora.com') return <IconQuora style={{ height: '28px', width: '28px', color: '#b82b27' }} />;
     if (platform === 'twitter.com') return <Twitter sx={{ height: '28px', width: '28px', color: '#17a3f1' }} />;
     if (platform === 'linkedin.com') return <LinkedIn sx={{ height: '28px', width: '28px', color: '#006699' }} />;
+    if (platform === 'tiktok.com') return <IconBrandTiktok sx={{ height: '28px', width: '28px', color: '#006699' }} />;
+    if (platform === 'facebook.com') return <Facebook sx={{ height: '28px', width: '28px', color: '#006699' }} />;
+    if (platform === 'instagram.com') return <Instagram sx={{ height: '28px', width: '28px', color: '#006699' }} />;
+    if (platform === 'pinterest.com') return <Pinterest sx={{ height: '28px', width: '28px', color: '#006699' }} />;
     return <></>;
 };
